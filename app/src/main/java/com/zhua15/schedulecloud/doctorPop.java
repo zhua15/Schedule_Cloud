1d8cec8642e906974861a326d1def25f26c64853
andy11181 <zhua15@mcmaster.ca>
2020-01-25 17:25:43 -0500

    changed layout

--- app/src/main/java/com/zhua15/schedulecloud/doctorPop.java
+++ app/src/main/java/com/zhua15/schedulecloud/doctorPop.java
@@ -1,6 +1,29 @@
 package com.zhua15.schedulecloud;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.TextView;
 
 public class doctorPop extends Activity {
+
+    TextView codeView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.doc_pop);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (0.75 * width), (int) (0.25 * height));
+
+        codeView = findViewById(R.id.codeView);
+        int i = (int) (Math.random() * 999999 + 100000);
+        codeView.setText("Doctor Code:\n" + i);
+    }
 }
--- app/src/main/java/com/zhua15/schedulecloud/patientPop.java
+++ app/src/main/java/com/zhua15/schedulecloud/patientPop.java
@@ -1,6 +1,22 @@
 package com.zhua15.schedulecloud;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
 
 public class patientPop extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pat_pop);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (0.75 * width), (int) (0.25 * height));
+    }
 }
