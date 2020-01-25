ebaca2f8a9c4e82ab2961f682330d91929d9b4e8
andy11181 <zhua15@mcmaster.ca>
2020-01-25 16:04:31 -0500

    changed layout

--- app/src/main/java/com/zhua15/schedulecloud/SignupActivity.java
+++ app/src/main/java/com/zhua15/schedulecloud/SignupActivity.java
@@ -1,13 +1,29 @@
 package com.zhua15.schedulecloud;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignupActivity extends Activity {
+
+    Button patient;
+    Button doctor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        patient = findViewById(R.id.patient);
+        doctor = findViewById(R.id.doctor);
     }
+    public void patient(View view) {
+        startActivity(new Intent(SignupActivity.this, patientPop.class));
+    }
+    public void doctor(View view) {
+        startActivity(new Intent(SignupActivity.this, doctorPop.class));
+    }
+
 }
---
+++ app/src/main/java/com/zhua15/schedulecloud/patientPop.java
@@ -0,0 +1,6 @@
+package com.zhua15.schedulecloud;
+
+import android.app.Activity;
+
+public class patientPop extends Activity {
+}
